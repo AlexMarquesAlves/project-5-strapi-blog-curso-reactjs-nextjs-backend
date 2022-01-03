37df96a08e2e1ca6331149d48d5539bd88a3f16d
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-03 16:52:04 -0300

    atualizando configs

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster1.qjai6.mongodb.net"),
-        srv: env.bool("DATABASE_SRV", true),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "strapi-blog"),
-        username: env("DATABASE_USERNAME", "user_admin"),
-        password: env("DATABASE_PASSWORD", "@kiraLeo2912"),
+        host: env("DATABASE_HOST"),
+        srv: env.bool("DATABASE_SRV"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
